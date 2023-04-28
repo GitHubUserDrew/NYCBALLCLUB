388556ab15b05da0537be28cef4639a1001e6d90
unknown <andrewjagpal@gmail.com>
2023-04-28 17:22:50 -0400

    added popup menu

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,15 +1,19 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import '../css/Home.css'
 import Mapbox from '../component/Mapbox'
 import { getParks } from './../store/parkSlice';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import Modal from "../component/Modal";
+import Park from "../component/Park"
+
 
 
 
 function HomePage() {
   const dispatch = useDispatch();
   const parks = useSelector(state => state.parks)
+  const [park , setPark]= useState(null)
 
   useEffect(()=>{
     dispatch(getParks())
@@ -22,7 +26,10 @@ function HomePage() {
   return (
    
     <div className="Home">
-      <Mapbox/>
+     
+
+      <Mapbox setPark={setPark}/>
+      {park && <Modal  close={()=> setPark(null)}><Park park={park}/></Modal>}
     </div>
   )
 }
--- client/src/component/Park.js
+++ client/src/component/Park.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import "../css/Park.css"
 
-function Park() {
+
+
+function Park({park}) {
+    const {name, image, long, lat} = park
   return (
     <div className="Park">
-      
+     <div className="info">
+        <div className="park-img">
+            <img src={park.image} alt="" />
+        </div>
+        <h1 className="park-name">{name}</h1>
+        <h2 className="park-rating">4 stars</h2>
+        <p className="park-address"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, autem!</p>
+
+     </div>
+     <div className="side">
+        <div className="park-heading">
+            <h2>Reviews</h2>
+            <h2>Posts</h2>
+        </div>
+        <div className="main">
+
+        </div>
+
+
+
+     </div>
     </div>
   )
 }
